a7d9711dee99a3aad4176604f56262bac323bc94
BBrandford11 <brandfordbryce@gmail.com>
2022-02-16 14:04:40 -0700

    first sname play

---
+++ play.js
@@ -0,0 +1,23 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: "165.227.47.243",
+    port: 50541
+  });
+  conn.on("connect", () => {
+    // code that does something when the connection is first established
+    console.log("YO you connected")
+  });
+  conn.on("data", (sent) => {
+    console.log(sent)
+  });
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
